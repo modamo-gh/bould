956c434192a503e68a7be0e43e152e545fb6ce2d
modamo-gh <morganthemosaic@gmail.com>
2025-01-04 18:43:00 -0500

    Display session data in a graph

--- src/contexts/ClimberDataContext.tsx
+++ src/contexts/ClimberDataContext.tsx
@@ -4,44 +4,44 @@ import { createContext, useContext, useEffect, useState } from "react";
 const ClimberDataContext = createContext({});
 
 export const ClimberDataProvider = ({ children }) => {
-    const [climberData, setClimberData] = useState();
+	const [climberData, setClimberData] = useState();
 
-    useEffect(() => {
-        (async () => {
-            try {
-                let storedClimberData = await AsyncStorage.getItem(
-                    "climberData"
-                );
+	useEffect(() => {
+		(async () => {
+			try {
+				let storedClimberData = await AsyncStorage.getItem(
+					"climberData"
+				);
 
-                storedClimberData = JSON.parse(storedClimberData) || {};
+				storedClimberData = JSON.parse(storedClimberData) || {};
 
-                setClimberData(storedClimberData);
-            } catch (error) {
-                console.log("Error retrieving data", error);
-            }
-        })();
-    }, []);
+				setClimberData(storedClimberData);
+			} catch (error) {
+				console.log("Error retrieving data", error);
+			}
+		})();
+	}, []);
 
-    const addSessionData = async (date, sessionData) => {
-        const newClimberData = { ...climberData };
+	const addSessionData = async (date, sessionData) => {
+		const newClimberData = { ...climberData };
 
-        newClimberData[date] = sessionData;
+		newClimberData[date] = sessionData;
 
-        setClimberData(newClimberData);
+		setClimberData(newClimberData);
 
-        await AsyncStorage.setItem(
-            "climberData",
-            JSON.stringify(newClimberData)
-        );
+		await AsyncStorage.setItem(
+			"climberData",
+			JSON.stringify(newClimberData)
+		);
 
-        console.log(date, sessionData, newClimberData)
-    };
+		console.log(date, sessionData, newClimberData);
+	};
 
-    return (
-        <ClimberDataContext.Provider value={{ addSessionData, climberData }}>
-            {children}
-        </ClimberDataContext.Provider>
-    );
+	return (
+		<ClimberDataContext.Provider value={{ addSessionData, climberData }}>
+			{children}
+		</ClimberDataContext.Provider>
+	);
 };
 
 export const useClimberData = () => useContext(ClimberDataContext);
--- src/screens/PastSessionsScreen.tsx
+++ src/screens/PastSessionsScreen.tsx
@@ -1,12 +1,79 @@
-import { SafeAreaView, Text } from "react-native";
+import { Dimensions, SafeAreaView, ScrollView, Text, View } from "react-native";
 import { useClimberData } from "../contexts/ClimberDataContext";
+import { BarGroup, CartesianChart } from "victory-native";
+import { useFont } from "@shopify/react-native-skia";
+import rockledge from "../../assets/fonts/Rockledge-9YYWB.otf";
 
 const PastSessionsScreen = () => {
 	const { climberData } = useClimberData();
 
+	const formattedData = Object.keys(climberData || {}).flatMap((session) =>
+		Object.keys(climberData[session]).map((grade) => ({
+			grade,
+			attempts:
+				climberData[session][grade]["sent"] +
+				climberData[session][grade]["didNotSend"],
+			sends: climberData[session][grade]["sent"]
+		}))
+	);
+
+	const stats = { totalAttempts: 0, totalSuccessfulAttempts: 0, cumulativeGrade: 0 };
+
+	formattedData.forEach((datum) => {
+		stats.cumulativeGrade += 0;
+		stats.totalAttempts += datum.attempts;
+		stats.totalSuccessfulAttempts += datum.sends;
+	});
+
+	const font = useFont(rockledge, 12);
+
+	const screenHeight = Dimensions.get("window").height;
+
 	return (
 		<SafeAreaView style={{ flex: 1, backgroundColor: "#B2BEB5" }}>
-			<Text>{JSON.stringify(climberData)}</Text>
+			{formattedData.length ? (
+				<ScrollView style={{ flex: 1 }}>
+					<View style={{ height: 0.65 * screenHeight, padding: 16 }}>
+						<CartesianChart
+							xAxis={{ font }}
+							data={formattedData}
+							domainPadding={{ left: 50, right: 50 }}
+							xKey={"grade"}
+							yKeys={["sends", "attempts"]}
+						>
+							{({ points, chartBounds }) => (
+								<BarGroup
+									chartBounds={chartBounds}
+									betweenGroupPadding={0.3}
+									withinGroupPadding={0.1}
+									roundedCorners={{ topLeft: 5, topRight: 5 }}
+								>
+									<BarGroup.Bar
+										points={points.sends}
+										color="green"
+									/>
+									<BarGroup.Bar
+										points={points.attempts}
+										color="red"
+									/>
+								</BarGroup>
+							)}
+						</CartesianChart>
+					</View>
+					<Text>
+						{`You attempted ${stats.totalAttempts} climbs during this timeframe`}
+					</Text>
+					<Text>
+						{`You sent ${
+							stats.totalSuccessfulAttempts
+						} climbs during this timeframe for a success rate of ${(
+							(100 * stats.totalSuccessfulAttempts) /
+							stats.totalAttempts
+						).toFixed(2)}%`}
+					</Text>
+					<Text>Your mean grade was</Text>
+				</ScrollView>
+			) : null}
 		</SafeAreaView>
 	);
 };
