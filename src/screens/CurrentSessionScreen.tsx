cf8089adde9755cd3b9474f02d3631c35cd95638
Bryan <morganthemosaic@gmail.com>
2025-01-13 09:24:57 -0500

    Define session key type

--- src/screens/CurrentSessionScreen.tsx
+++ src/screens/CurrentSessionScreen.tsx
@@ -10,7 +10,7 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 let gradePassFailMap = {};
 
-let sessionKey;
+let sessionKey: string;
 
 const CurrentSessionScreen = () => {
 	const [isFirstClimb, setIsFirstClimb] = useState(true);
