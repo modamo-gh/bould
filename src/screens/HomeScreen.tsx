9c6267816fe8ae56070b92f7eaf4747b27ee9406
modamo-gh <morganthemosaic@gmail.com>
2025-04-21 19:02:21 -0400

    Change image and add two buttons to replace tabs

--- src/screens/HomeScreen.tsx
+++ src/screens/HomeScreen.tsx
@@ -1,17 +1,20 @@
 import { LinearGradient } from "expo-linear-gradient";
-import {
-	Image,
-	ImageBackground,
-	SafeAreaView,
-	StyleSheet,
-	View
-} from "react-native";
+import { Image, ImageBackground, StyleSheet, Text, View } from "react-native";
+import { useFonts } from "expo-font";
 
 const HomeScreen = () => {
+	const [loaded, error] = useFonts({
+		Rockledge: require("../../assets/fonts/Rockledge-9YYWB.otf")
+	});
+
+	if (!loaded && !error) {
+		return null;
+	}
+
 	return (
 		<View style={{ backgroundColor: "#F4EBDC", flex: 1 }}>
 			<ImageBackground
-				resizeMode="center"
+				resizeMode="cover"
 				source={require("../../assets/climber.png")}
 				style={{
 					width: "100%",
@@ -22,13 +25,65 @@ const HomeScreen = () => {
 			>
 				<View style={{ flex: 4 }}></View>
 				<LinearGradient
-					colors={["rgba(244, 235, 220, 0)", "rgba(244, 235, 220, 1)"]}
-                    style={{flex: 1, alignItems: "center", width: "100%"}}
-                    start={{x: 0, y: 0}}
-                    end={{x: 0, y: 1}}
+					colors={[
+						"rgba(244, 235, 220, 0)",
+						"rgba(244, 235, 220, 1)"
+					]}
+					style={{ flex: 1, alignItems: "center", width: "100%" }}
+					start={{ x: 0, y: 0 }}
+					end={{ x: 0, y: 1 }}
 				/>
 			</ImageBackground>
-			<View style={{ flex: 1 }}></View>
+			<View
+				style={{
+					display: "flex",
+					flex: 1,
+					gap: 18,
+					justifyContent: "flex-end",
+					padding: 20
+				}}
+			>
+				<View
+					style={{
+						alignItems: "center",
+						backgroundColor: "#D62400",
+						borderRadius: 16,
+						display: "flex",
+						height: 85,
+						justifyContent: "center",
+						width: "100%"
+					}}
+				>
+					<Text
+						style={{
+							color: "#F4ECDD",
+							fontFamily: "Rockledge",
+							fontSize: 44,
+						}}
+					>
+						START SESSION
+					</Text>
+				</View>
+				<View
+					style={{
+						alignItems: "center",
+						backgroundColor: "#502314",
+						borderRadius: 16,
+						display: "flex",
+						height: 85,
+						justifyContent: "center",
+						width: "100%"
+					}}
+				><Text
+					style={{
+						color: "#F4ECDD",
+						fontFamily: "Rockledge",
+						fontSize: 44,
+					}}
+				>
+						PAST STATS
+					</Text></View>
+			</View>
 		</View>
 	);
 };
